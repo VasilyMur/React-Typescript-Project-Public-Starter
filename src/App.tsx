3d3bf3807c716d5bccb999ad0b8585df13e85b2c
alexgold71 <80583194+alexgold71@users.noreply.github.com>
2023-03-02 16:22:29 +0300

    1 редакция

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
 
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      New App!
+      <div className='wrap'>
+        <span className='heading'>Наша пиццерия</span>
+      </div>
     </div>
   );
 }
